06234fd8db4bfdfc4fc2dde6a6c22d1ee4b3b5a8
Anissa Crawford <anissacrawford@gmail.com>
2022-02-15 09:59:47 -0600

    answered 4 and 5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -44,6 +44,8 @@ console.log(me.favThreeFoods[2]);
   - Console.log your updated shoe count.
 */
 
+console.log(me.shoeCount);
+console.log(me.shoeCount+1);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -51,3 +53,5 @@ console.log(me.favThreeFoods[2]);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+console.log(me.favoriteColor='green'); //why is it equal sign here and not colon
